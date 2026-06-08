4987fc9e3199d03f55d89631bb2d7f45fa441cdb
Oran Glikman <glikman08@gmail.com>
2026-06-08 16:06:18 +0300

    Add proxy trust for Passport in production

--- server.js
+++ server.js
@@ -108,7 +108,8 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'YOUR_GOOGLE_CLIENT_SECRET',
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
   },
   (accessToken, refreshToken, profile, done) => {
     const { id, displayName, emails } = profile;
